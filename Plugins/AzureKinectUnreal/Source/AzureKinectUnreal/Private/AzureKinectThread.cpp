6e8ec0b07f3b0dc367a1b3d703990e0014b5709b
Tejaswin G Macharla <tmacharla@secretlocation.com>
2019-10-22 17:03:25 -0400

    - Added basic functions for multi threading

--- Plugins/AzureKinectUnreal/Source/AzureKinectUnreal/Private/AzureKinectThread.cpp
+++ Plugins/AzureKinectUnreal/Source/AzureKinectUnreal/Private/AzureKinectThread.cpp
@@ -2,11 +2,87 @@
 
 
 #include "AzureKinectThread.h"
+#include "HAL/PlatformProcess.h"
 
-FAzureKinectThread::FAzureKinectThread()
+DEFINE_LOG_CATEGORY(AzureKinectThreadLog);
+
+FAzureKinectThread *FAzureKinectThread::Instance = nullptr;
+
+FAzureKinectThread::FAzureKinectThread() :
+	KinectThread(nullptr),
+	StopThreadCounter(0)
 {
+	KinectThread = FRunnableThread::Create(this, TEXT("FAzureKinectThread"), 0, TPri_BelowNormal); //windows default = 8mb for thread, could specify more
+	if (!KinectThread)
+	{
+		UE_LOG(AzureKinectThreadLog, Error, TEXT("Failed to create Azure Kinect thread."));
+	}
 }
 
 FAzureKinectThread::~FAzureKinectThread()
 {
+	if (KinectThread)
+	{
+		delete KinectThread;
+		KinectThread = nullptr;
+	}
+}
+
+FAzureKinectThread * FAzureKinectThread::InitPolling()
+{
+	//Create new instance of thread if it does not exist
+	//		and the platform supports multi threading!
+	if (!Instance && FPlatformProcess::SupportsMultithreading())
+	{
+		Instance = new FAzureKinectThread();
+	}
+	return Instance;
+}
+
+void FAzureKinectThread::Shutdown()
+{
+	if (Instance)
+	{
+		Instance->EnsureCompletion();
+		delete Instance;
+		Instance = nullptr;
+	}
+}
+
+//bool FAzureKinectThread::IsThreadFinished()
+//{
+//	if (Instance) return Instance->IsFinished();
+//	return true;
+//}
+
+void FAzureKinectThread::EnsureCompletion()
+{
+	Stop();
+	if (KinectThread)
+	{
+		KinectThread->WaitForCompletion();
+	}
+}
+
+bool FAzureKinectThread::Init()
+{
+	UE_LOG(AzureKinectThreadLog, Log, TEXT("Azure Kinect thread started."));
+	return false;
+}
+
+uint32 FAzureKinectThread::Run()
+{
+	UE_LOG(AzureKinectThreadLog, Log, TEXT("Azure Kinect thread running."));
+
+	while (StopThreadCounter.GetValue() == 0)
+	{
+
+	}
+
+	return 0;
+}
+
+void FAzureKinectThread::Stop()
+{
+	StopThreadCounter.Increment();
 }
--- Plugins/AzureKinectUnreal/Source/AzureKinectUnreal/Public/AzureKinectThread.h
+++ Plugins/AzureKinectUnreal/Source/AzureKinectUnreal/Public/AzureKinectThread.h
@@ -3,13 +3,40 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "HAL/Runnable.h"
+#include "HAL/RunnableThread.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(AzureKinectThreadLog, Log, All);
 
 /**
  * 
  */
-class AZUREKINECTUNREAL_API FAzureKinectThread
+class AZUREKINECTUNREAL_API FAzureKinectThread : public FRunnable
 {
 public:
 	FAzureKinectThread();
 	~FAzureKinectThread();
+
+	static FAzureKinectThread *InitPolling();
+
+	static void Shutdown();
+
+	//static bool IsThreadFinished();
+	
+	void EnsureCompletion();
+
+	// Begin FRunnable interface
+	virtual bool Init();
+	virtual uint32 Run();
+	virtual void Stop();
+	// End FRunnable interface
+
+private:
+	static FAzureKinectThread *Instance;
+
+	FRunnableThread *KinectThread;
+
+	FThreadSafeCounter StopThreadCounter;
+
+
 };
