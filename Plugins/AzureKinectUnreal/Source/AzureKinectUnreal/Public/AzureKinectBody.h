3251d1ee36df78ea176b46f21d29a2c89ef77b3b
Tejaswin G Macharla <tmacharla@secretlocation.com>
2019-10-25 16:58:48 -0400

    - Added AzureKinectBody that will be used to track individual bodies and will be blueprintable

---
+++ Plugins/AzureKinectUnreal/Source/AzureKinectUnreal/Public/AzureKinectBody.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AzureKinectBody.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZUREKINECTUNREAL_API UAzureKinectBody : public UObject
+{
+	GENERATED_BODY()
+	
+};
